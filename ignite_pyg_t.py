9821fb2f94e1578683b823fee91fa6bde4717724
htlee6 <hauten.lee@gmail.com>
2022-04-28 21:31:23 +0000

    update

--- ignite_pyg_t.py
+++ ignite_pyg_t.py
@@ -6,7 +6,7 @@ from numpy import dtype
 import torch
 from ignite.engine import Engine, Events
 from torch.nn import NLLLoss
-from torch.optim import SGD
+from torch.optim import SGD, Adam
 from torch.utils.data import Dataset
 
 # rom torch_geometric.datasets import Planetoid
@@ -56,7 +56,7 @@ class Collater(object):
 
 # Custmoize your model
 
-from GNN import Net
+from gnn import GNN
 
 def training(rank, config):
 
@@ -84,18 +84,39 @@ def training(rank, config):
 
     # Specific ignite.distributed
     # train_loader = idist.auto_dataloader(dataset, batch_size=config["batch_size"], collate_fn=Collater(False, []))
-    dataset = PygGraphPropPredDataset(name='ogbg-molhiv', root='datas/')
+    # print(config['dataset'])
+    dataset = PygGraphPropPredDataset(name=config['dataset'])
+    
+    split_idx = dataset.get_idx_split()
     # print()
-    train_loader = idist.auto_dataloader(dataset, batch_size=config['batch_size'], collate_fn=Collater(False, []))
+    train_loader = idist.auto_dataloader(dataset[split_idx['train']], batch_size=config['batch_size'],shuffle=True, collate_fn=Collater(False, []))
+    valid_loader = idist.auto_dataloader(dataset[split_idx['valid']], batch_size=config['batch_size'],shuffle=True, collate_fn=Collater(False, []))
+    test_loader = idist.auto_dataloader(dataset[split_idx['test']], batch_size=config['batch_size'],shuffle=True, collate_fn=Collater(False, []))
 
     # Model, criterion, optimizer setup
-    model = idist.auto_model(Net(16))
-    criterion = NLLLoss()
-    optimizer = idist.auto_optim(SGD(model.parameters(), lr=0.01))
+    if config['gnn'] == 'gin':
+        model = GNN(gnn_type = 'gin', num_tasks = dataset.num_tasks, num_layer = config['num_layer'], emb_dim = config['emb_dim'], drop_ratio = config['drop_ratio'], virtual_node = False).to(device)
+    elif config['gnn'] == 'gin-virtual':
+        model = GNN(gnn_type = 'gin', num_tasks = dataset.num_tasks, num_layer = config['num_layer'], emb_dim = config['emb_dim'], drop_ratio = config['drop_ratio'], virtual_node = True).to(device)
+    elif config['gnn'] == 'gcn':
+        model = GNN(gnn_type = 'gcn', num_tasks = dataset.num_tasks, num_layer = config['num_layer'], emb_dim = config['emb_dim'], drop_ratio = config['drop_ratio'], virtual_node = False).to(device)
+    elif config['gnn'] == 'gcn-virtual':
+        model = GNN(gnn_type = 'gcn', num_tasks = dataset.num_tasks, num_layer = config['num_layer'], emb_dim = config['emb_dim'], drop_ratio = config['drop_ratio'], virtual_node = True).to(device)
+    else:
+        raise ValueError('Invalid GNN type')
+   
+    # print("Preparing Model")
+    model = idist.auto_model(model)
+    # print("Preparing Opt")
+    optimizer = idist.auto_optim(Adam(model.parameters(), lr=0.01))
+    if dataset.task_type == 'classification':
+        criterion = torch.nn.BCEWithLogitsLoss()
+    else:
+        criterion = torch.nn.MSELoss()
 
     # Training loop log param
     log_interval = config["log_interval"]
-
+    '''
     def _train_step(engine, batch):
 
         data = batch
@@ -114,14 +135,36 @@ def training(rank, config):
         optimizer.step()
 
         return loss_val
+    '''
+
+    def _train(engine, foo):
+        model.train()
+        for step, batch in enumerate(train_loader):
+            # if step % 100 == 0:
+            #     print("current step " + str(step))
+            batch = batch.to(device)
+
+            if batch.x.shape[0] == 1 or batch.batch[-1] == 0:
+                pass
+            else:
+                pred = model(batch)
+                optimizer.zero_grad()
+                ## ignore nan targets (unlabeled) when computing training loss.
+                is_labeled = batch.y == batch.y
+                loss = criterion(pred.to(torch.float32)[is_labeled], batch.y.to(torch.float32)[is_labeled])
+                loss.backward()
+                optimizer.step()
 
     # Running the _train_step function on whole batch_data iterable only once
-    trainer = Engine(_train_step)
+    # trainer = Engine(_train_step)
+    # trainer = Engine(train, {"model": model, "device": device, "loader": train_loader, "optimizer": optimizer})
+    # print("About to Start")
+    trainer = Engine(_train)
 
     # Add a logger
-    @trainer.on(Events.ITERATION_COMPLETED(every=1000))
-    def log_training():
-        
+    @trainer.on(Events.ITERATION_COMPLETED(every=100))
+    def log_training(engine):
+        '''
         print(
             "Process {}/{} Train Epoch: {} [{}/{}]\tLoss: {}".format(
                 idist.get_rank(),
@@ -131,8 +174,16 @@ def training(rank, config):
                 len(dataset) / idist.get_world_size(),
                 trainer.state.output,
             )
-        )
         
+        )
+        '''
+        batch_loss = engine.state.output
+        lr = optimizer.param_groups[0]['lr']
+        e = engine.state.epoch
+        n = engine.state.max_epochs
+        i = engine.state.iteration
+        print(f"Epoch {e}/{n} : {i} - batch loss: {batch_loss}, lr: {lr}")
+
 
     trainer.run(train_loader, max_epochs=10)
 
@@ -143,17 +194,37 @@ if __name__ == "__main__":
     parser.add_argument("--nproc_per_node", type=int)
     parser.add_argument("--log_interval", type=int, default=4)
     parser.add_argument("--nb_samples", type=int, default=128)
-    parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--nnodes", type=int)
     parser.add_argument("--node_rank", type=int)
     parser.add_argument("--master_addr", type=str)
     parser.add_argument("--master_port", type=int)
+    parser.add_argument('--device', type=int, default=0,
+                        help='which gpu to use if any (default: 0)')
+    parser.add_argument('--gnn', type=str, default='gin-virtual',
+                        help='GNN gin, gin-virtual, or gcn, or gcn-virtual (default: gin-virtual)')
+    parser.add_argument('--drop_ratio', type=float, default=0.5,
+                        help='dropout ratio (default: 0.5)')
+    parser.add_argument('--num_layer', type=int, default=5,
+                        help='number of GNN message passing layers (default: 5)')
+    parser.add_argument('--emb_dim', type=int, default=300,
+                        help='dimensionality of hidden units in GNNs (default: 300)')
+    parser.add_argument('--batch_size', type=int, default=32,
+                        help='input batch size for training (default: 32)')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='number of epochs to train (default: 100)')
+    parser.add_argument('--num_workers', type=int, default=0,
+                        help='number of workers (default: 0)')
+    parser.add_argument('--dataset', type=str, default="ogbg-molhiv",
+                        help='dataset name (default: ogbg-molhiv)')
+
+    parser.add_argument('--feature', type=str, default="full",
+                        help='full feature or simple feature')
+    parser.add_argument('--filename', type=str, default="",
+                        help='filename to output result (default: )')
+ 
     args_parsed = parser.parse_args()
 
-    # torch.set_default_dtype(torch.float64)
-
-    # dataset = Planetoid('datas', 'Cora') 
-    # data = dataset[0]
+    device = torch.device("cuda:" + str(args_parsed.device)) if torch.cuda.is_available() else torch.device("cpu")
 
     # idist from ignite handles multiple backend (gloo, nccl, horovod, xla)
     # and launcher (torch.distributednch, horovodrun, slurm)
@@ -161,6 +232,12 @@ if __name__ == "__main__":
         "log_interval": args_parsed.log_interval,
         "batch_size": args_parsed.batch_size,
         "nb_samples": args_parsed.nb_samples,
+        "gnn": args_parsed.gnn,
+        "dataset": args_parsed.dataset,
+        "num_layer": args_parsed.num_layer,
+        "emb_dim": args_parsed.emb_dim,
+        "drop_ratio": args_parsed.drop_ratio
+        
     }
 
     spawn_kwargs = dict()
